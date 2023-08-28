f925e2e0bfe886197290b3f96347c0a9abd77736
Corey Gallagher <coreygallagher1194@gmail.com>
2023-08-28 17:27:46 -0500

    add browser icon

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        <link rel='shortcut icon' href='/Corey3.png' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
